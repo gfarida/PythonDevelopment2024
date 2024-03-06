66574d963d7f2e449083bd7578641897b013c2ee
Farida Gaifutdinova <farida.gaif@ispras.ru>
2024-03-06 17:38:44 +0300

    cowsay base version, without cowfile and wrap

---
+++ 02_PushPip/cow_say.py
@@ -0,0 +1,53 @@
+import argparse
+import cowsay
+
+def main():
+    parser = argparse.ArgumentParser(description='Python implementation of cowsay')
+    parser.add_argument('message', nargs='?', help='Message to display (default is "Hello")')
+    parser.add_argument('-f', '--file', metavar='cowfile', default='default', help='Specify the cowfile to use')
+    parser.add_argument('-e', '--eyes', metavar='eye_string', default='oo', help='Change the cow\'s eyes')
+    parser.add_argument('-T', '--tongue', metavar='tongue_string',  default='  ', help='Change the cow\'s tongue')
+    parser.add_argument('-W', '--width', metavar='wrapcolumn', type=int,  default=40, help='Specify line width')
+    parser.add_argument('-l', '--list', action='store_true', help='List available cowfiles')
+    parser.add_argument('-b', '--borg', action='store_true', help='Borg mode')
+    parser.add_argument('-d', '--dead', action='store_true', help='Make the cow appear dead')
+    parser.add_argument('-g', '--greedy', action='store_true', help='Invoke greedy mode')
+    parser.add_argument('-p', '--paranoid', action='store_true', help='Cause a state of paranoia')
+    parser.add_argument('-s', '--stoned', action='store_true', help='Make the cow appear stoned')
+    parser.add_argument('-t', '--tired', action='store_true', help='Yields a tired cow')
+    parser.add_argument('-w', '--wired', action='store_true', help='Initiates wired mode')
+    parser.add_argument('-y', '--youthful', action='store_true', help='Brings on the cow\'s youthful appearance')
+    args = parser.parse_args()
+
+    if args.list:
+        print(cowsay.list_cows())
+        return
+    
+    if args.file != 'default':
+        cowsay.get_cow(args.file)
+
+    message = args.message if args.message else "Hello"
+    mode = ''
+    if args.dead:
+        mode = 'd'
+    elif args.greedy:
+        mode = 'g'
+    elif args.paranoid:
+        mode = 'p'
+    elif args.stoned:
+        mode = 's'
+    elif args.tired:
+        mode = 't'
+    elif args.wired:
+        mode = 'w'
+    elif args.youthful:
+        mode = 'y'
+    
+    for i in cowsay.list_cows():
+        args.file = i
+        cowsay.get_cow(args.file)
+        print(i)
+        print(cowsay.cowsay(message, cow=args.file, eyes=args.eyes, tongue=args.tongue, width=args.width, preset=mode))
+
+if __name__ == '__main__':
+    main()
