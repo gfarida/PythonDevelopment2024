0d2fde4574369870305eb91aa4fc179bcc26a940
gfarida <faridafarida378@gmail.com>
2024-03-18 16:36:33 +0300

    HW4: Addedcowsay function

--- 04_MergetoolCommandline/cowsay_cmd.py
+++ 04_MergetoolCommandline/cowsay_cmd.py
@@ -2,50 +2,38 @@ import cmd
 import shlex
 import cowsay
 
+EYES = ['oo', '$$', '**', '//', '--']
+TONGUE = ['UU', 'WW', 'JJ', 'PP']
+
 class CowCmd(cmd.Cmd):
-    prompt = ">> "
-    intro = "Welcome to CowCmd! Type help or ? to list commands.\n"
+    prompt = "cmd>> "
 
     def do_list_cows(self, args):
-        """List available cows."""
-        print("Available cows:")
+        """List of available cows
+           Usage: list_cows """
+        print("Cows:", end=' ')
         print(", ".join(cowsay.list_cows()))
 
-    def do_make_bubble(self, args):
-        """Make a bubble."""
-        print("Making a bubble...")
-
     def do_cowsay(self, args):
-        """Make the cow say something."""
+        """
+        Make the cow say something
+        Usage: cowsay message [cow] [eyes] [tongue]
+        """
         try:
             args = shlex.split(args)
             message = args[0] if args else "Hello"
-            cow = args[1] if len(args) > 1 else None
+            cow = args[1] if len(args) > 1 else 'default'
             eyes = args[2] if len(args) > 2 else "oo"
-            tongue = args[3] if len(args) > 3 else "  "
+            tongue = args[3] if len(args) > 3 else "U"
             print(cowsay.cowsay(message, cow=cow, eyes=eyes, tongue=tongue))
         except ValueError as e:
             print("Error:", e)
 
-    def do_cowthink(self, args):
-        """Make the cow think something."""
-        try:
-            args = shlex.split(args)
-            message = args[0] if args else "Hmm..."
-            cow = args[1] if len(args) > 1 else None
-            eyes = args[2] if len(args) > 2 else "oo"
-            tongue = args[3] if len(args) > 3 else "  "
-            print(cowsay.cowthink(message, cow=cow, eyes=eyes, tongue=tongue))
-        except ValueError as e:
-            print("Error:", e)
-
     def do_quit(self, args):
-        """Exit the program."""
+        """Exit the program"""
         print("Quitting...")
         return True
 
-    def help_quit(self):
-        print("Exit the program.")
 
 if __name__ == "__main__":
     CowCmd().cmdloop()
